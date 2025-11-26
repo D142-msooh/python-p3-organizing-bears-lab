02e1e12e717aa6b3074de5bbdd9fefa7aadaeedc
D142-msooh <kinyajuidama176@gmail.com>
2025-11-26 22:55:02 +0300

    Add create.sql, insert.sql, sql_queries.py

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Mr.chocolate', 20, 'M', 'Orange', 'friendly', 1),
+    ('Rowdy', 12, 'M', 'Black', 'calm', 0),
+    ('Tabitha', 24, 'F', 'Blue', 'aggressive', 1),
+    ('Sergent Brown', 51, 'M', 'Brown', 'serious', 1),
+    ('Melissa', 37, 'F', 'Pink', 'Jovial', 1),
+    ('Grinch', 43, 'M', 'White', 'kind', 0),
+    ('Wendy', 28, 'F', 'Yellow', 'shy', 1),
+    (NULL, 69, 'M', 'grey', 'happpy', 1);
